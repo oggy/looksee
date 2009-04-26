baa8fcbd404830049586167c291eb69961b4725a
George Ogata <george.ogata@gmail.com>
2009-04-26 19:30:57 +1000

    Use old-style C comments.

--- ext/looksee/looksee.c
+++ ext/looksee/looksee.c
@@ -1,38 +1,37 @@
 #include "ruby.h"
 
-//
-// Return the internal superclass of this class.
-//
-// This is either a Class or "IClass."  IClasses represent Modules
-// included in the ancestry, and should be treated as opaque objects
-// in ruby space. Convert the IClass to a Module using #iclass_to_module
-// before using it in ruby.
-//
+/*
+ * Return the internal superclass of this class.
+ *
+ * This is either a Class or "IClass."  IClasses represent Modules
+ * included in the ancestry, and should be treated as opaque objects
+ * in ruby space. Convert the IClass to a Module using #iclass_to_module
+ * before using it in ruby.
+ */
 VALUE Looksee_internal_superclass(VALUE self, VALUE internal_class) {
   VALUE super = RCLASS(internal_class)->super;
-  // return BUILTIN_TYPE(super) == T_ICLASS ? RBASIC(super)->klass : super;
   if (!super)
     return Qnil;
   return super;
 }
 
-//
-// Return the internal class of the given object.
-//
-// This is either the object's singleton class, if it exists, or the
-// object's birth class.
-//
+/*
+ * Return the internal class of the given object.
+ *
+ * This is either the object's singleton class, if it exists, or the
+ * object's birth class.
+ */
 VALUE Looksee_internal_class(VALUE self, VALUE object) {
   return RBASIC(object)->klass;
 }
 
-//
-// Return the class or module that the given internal class
-// represents.
-//
-// If a class is given, this is the class.  If an iclass is given,
-// this is the module it represents in the lookup chain.
-//
+/*
+ * Return the class or module that the given internal class
+ * represents.
+ *
+ * If a class is given, this is the class.  If an iclass is given,
+ * this is the module it represents in the lookup chain.
+ */
 VALUE Looksee_internal_class_to_module(VALUE self, VALUE internal_class) {
   if (!SPECIAL_CONST_P(internal_class)) {
     switch (BUILTIN_TYPE(internal_class)) {
@@ -45,9 +44,6 @@ VALUE Looksee_internal_class_to_module(VALUE self, VALUE internal_class) {
   rb_raise(rb_eArgError, "not an internal class: %s", RSTRING(rb_inspect(internal_class))->ptr);
 }
 
-//
-// Library initializer.
-//
 void Init_looksee(void) {
   VALUE mLooksee = rb_define_module("Looksee");
   rb_define_singleton_method(mLooksee, "internal_superclass", Looksee_internal_superclass, 1);
