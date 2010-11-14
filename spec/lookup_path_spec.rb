2c20a513468c9678d7da38445c0d29d9fa2251ef
George Ogata <george.ogata@gmail.com>
2010-11-13 19:35:14 -0600

    Refactor tests a little.

--- spec/lookup_path_spec.rb
+++ spec/lookup_path_spec.rb
@@ -95,94 +95,87 @@ describe Looksee::LookupPath do
     before do
       Looksee.stubs(:default_lookup_path_options).returns({})
       Looksee.stubs(:styles).returns(Hash.new{'%s'})
+
+      @object = Object.new
+      temporary_module :M
+      temporary_class(:C) { include M }
+      Looksee.stubs(:lookup_modules).with(@object).returns([C, M])
     end
 
-    describe "contents" do
-      before do
-        temporary_module :M
-        temporary_class :C do
-          include M
-        end
-        @object = Object.new
-        Looksee.stubs(:lookup_modules).with(@object).returns([C, M])
-        stub_methods(C, ['public1', 'public2'], ['protected1', 'protected2'], ['private1', 'private2'], ['undefined1', 'undefined2'])
-        stub_methods(M, ['public1', 'public2'], ['protected1', 'protected2'], ['private1', 'private2'], ['undefined1', 'undefined2'])
-      end
-
-      it "should columnize each module's output to the current terminal width" do
-        original_columns = ENV['COLUMNS']
-        ENV['COLUMNS'] = '20'
-        begin
-          stub_methods(C, ['aa', 'bb', 'cc', 'dd', 'ee', 'ff', 'gg', 'hh', 'ii', 'jj'], [], [], [])
-          stub_methods(M, ['aaa', 'bbb', 'ccc', 'ddd', 'eee', 'fff', 'ggg', 'hhh'], [], [], [])
-          lookup_path = Looksee::LookupPath.new(@object, :public => true)
-          lookup_path.inspect.should == <<-EOS.demargin.chomp
-            |C
-            |  aa  cc  ee  gg  ii
-            |  bb  dd  ff  hh  jj
-            |M
-            |  aaa  ccc  eee  ggg
-            |  bbb  ddd  fff  hhh
-          EOS
-        ensure
-          ENV['COLUMNS'] = original_columns
-        end
-      end
-
-      it "should show singleton classes as class names in brackets" do
-        Looksee.stubs(:lookup_modules).with(C).returns([C.singleton_class])
-        stub_methods(C.singleton_class, ['public1', 'public2'], [], [], [])
-        lookup_path = Looksee::LookupPath.new(C, :public => true)
-        lookup_path.inspect.should == <<-EOS.demargin.chomp
-          |[C]
-          |  public1  public2
-        EOS
-      end
-
-      it "should handle singleton classes of singleton classes correctly" do
-        Looksee.stubs(:lookup_modules).with(C.singleton_class).returns([C.singleton_class.singleton_class])
-        stub_methods(C.singleton_class.singleton_class, ['public1', 'public2'], [], [], [])
-        lookup_path = Looksee::LookupPath.new(C.singleton_class, :public => true)
-        lookup_path.inspect.should == <<-EOS.demargin.chomp
-          |[[C]]
-          |  public1  public2
-        EOS
-      end
-
-      it "should not show any blank lines if a module has no methods" do
-        stub_methods(C, [], [], [], [])
-        lookup_path = Looksee::LookupPath.new(@object, :public => true, :overridden => true)
+    it "should columnize each module's output to the current terminal width" do
+      original_columns = ENV['COLUMNS']
+      ENV['COLUMNS'] = '20'
+      begin
+        stub_methods(C, ['aa', 'bb', 'cc', 'dd', 'ee', 'ff', 'gg', 'hh', 'ii', 'jj'], [], [], [])
+        stub_methods(M, ['aaa', 'bbb', 'ccc', 'ddd', 'eee', 'fff', 'ggg', 'hhh'], [], [], [])
+        lookup_path = Looksee::LookupPath.new(@object, :public => true)
         lookup_path.inspect.should == <<-EOS.demargin.chomp
           |C
+          |  aa  cc  ee  gg  ii
+          |  bb  dd  ff  hh  jj
           |M
-          |  public1  public2
+          |  aaa  ccc  eee  ggg
+          |  bbb  ddd  fff  hhh
         EOS
+      ensure
+        ENV['COLUMNS'] = original_columns
       end
     end
 
-    describe "styles" do
-      before do
-        styles = {
-          :module     => "`%s'",
-          :public     => "{%s}",
-          :protected  => "[%s]",
-          :private    => "<%s>",
-          :undefined  => "~%s~",
-          :overridden => "(%s)",
-        }
-        Looksee.stubs(:styles).returns(styles)
-      end
+    it "should not show any blank lines if a module has no methods" do
+      stub_methods(C, [], [], [], [])
+      stub_methods(M, ['public1', 'public2'], [], [], [])
+      lookup_path = Looksee::LookupPath.new(@object, :public => true, :overridden => true)
+      lookup_path.inspect.should == <<-EOS.demargin.chomp
+        |C
+        |M
+        |  public1  public2
+      EOS
+    end
 
-      it "should delimit each word with the configured delimiters" do
-        temporary_class :C
-        Looksee.stubs(:lookup_modules).returns([C])
-        stub_methods(C, ['public'], ['protected'], ['private'], ['undefined'])
-        lookup_path = Looksee::LookupPath.new(Object.new, :public => true, :protected => true, :private => true, :undefined => true, :overridden => true)
-        lookup_path.inspect.should == <<-EOS.demargin.chomp
+    it "should show singleton classes as class names in brackets" do
+      Looksee.stubs(:lookup_modules).with(C).returns([C.singleton_class])
+      stub_methods(C.singleton_class, ['public1', 'public2'], [], [], [])
+      lookup_path = Looksee::LookupPath.new(C, :public => true)
+      lookup_path.inspect.should == <<-EOS.demargin.chomp
+        |[C]
+        |  public1  public2
+      EOS
+    end
+
+    it "should handle singleton classes of singleton classes correctly" do
+      Looksee.stubs(:lookup_modules).with(C.singleton_class).returns([C.singleton_class.singleton_class])
+      stub_methods(C.singleton_class.singleton_class, ['public1', 'public2'], [], [], [])
+      lookup_path = Looksee::LookupPath.new(C.singleton_class, :public => true)
+      lookup_path.inspect.should == <<-EOS.demargin.chomp
+        |[[C]]
+        |  public1  public2
+      EOS
+    end
+  end
+
+  describe ".styles" do
+    before do
+      styles = {
+        :module     => "`%s'",
+        :public     => "{%s}",
+        :protected  => "[%s]",
+        :private    => "<%s>",
+        :undefined  => "~%s~",
+        :overridden => "(%s)",
+      }
+      Looksee.stubs(:styles).returns(styles)
+    end
+
+    it "should delimit each word with the configured delimiters" do
+      temporary_class :C
+      Looksee.stubs(:lookup_modules).returns([C])
+      stub_methods(C, ['public'], ['protected'], ['private'], ['undefined'])
+      lookup_path = Looksee::LookupPath.new(Object.new, :public => true, :protected => true, :private => true, :undefined => true, :overridden => true)
+      lookup_path.inspect.should == <<-EOS.demargin.chomp
           |\`C\'
           |  <private>  [protected]  {public}  ~undefined~
         EOS
-      end
     end
   end
 end
