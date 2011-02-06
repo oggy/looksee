979fac809cedeb629e1a413e222e9c431790fdae
George Ogata <george.ogata@gmail.com>
2011-02-06 05:59:03 -0500

    Load MRI extension from installed path.

--- lib/looksee/adapter.rb
+++ lib/looksee/adapter.rb
@@ -3,7 +3,7 @@ require 'rbconfig'
 module Looksee
   module Adapter
     autoload :Base, 'looksee/adapter/base'
-    autoload :MRI, File.dirname(__FILE__) + "/../../ext/mri/mri.#{Config::CONFIG['DLEXT']}"
+    autoload :MRI, "looksee/mri.#{Config::CONFIG['DLEXT']}"
     autoload :JRuby, 'looksee/looksee.jar'
     autoload :Rubinius, "looksee/adapter/rubinius"
   end
