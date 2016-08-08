1a0e26599e8b4d7318f28d476dac1f8cfe544db6
George Ogata <george.ogata@gmail.com>
2016-08-08 02:51:50 -0400

    Clean up java imports.

--- ext/jruby/looksee/JRubyAdapter.java
+++ ext/jruby/looksee/JRubyAdapter.java
@@ -1,30 +1,16 @@
 package looksee;
 
-import java.lang.reflect.Field;
 import java.util.Map;
 import org.jruby.MetaClass;
 import org.jruby.Ruby;
 import org.jruby.RubyArray;
 import org.jruby.RubyClass;
-import org.jruby.RubyFixnum;
-import org.jruby.RubyMethod;
 import org.jruby.RubyModule;
 import org.jruby.RubyObject;
-import org.jruby.RubyProc;
-import org.jruby.RubyString;
-import org.jruby.RubyUnboundMethod;
-import org.jruby.IncludedModuleWrapper;
 import org.jruby.anno.JRubyClass;
 import org.jruby.anno.JRubyMethod;
-import org.jruby.internal.runtime.methods.AliasMethod;
 import org.jruby.internal.runtime.methods.DynamicMethod;
-import org.jruby.internal.runtime.methods.MethodMethod;
-import org.jruby.internal.runtime.methods.ProcMethod;
-import org.jruby.internal.runtime.methods.WrapperMethod;
-import org.jruby.lexer.yacc.ISourcePosition;
-import org.jruby.runtime.PositionAware;
 import org.jruby.runtime.ThreadContext;
-import org.jruby.runtime.Visibility;
 import org.jruby.runtime.builtin.IRubyObject;
 
 @JRubyClass(name = "Looksee::Adapter::JRuby")
